8f3d27b3daec846143082f894b851d5389716f25
prudvendraBitcare <Prudvendra@gobitcare.com>
2022-11-07 19:56:07 +0530

    changed bitcare health care  spelling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
 
 export default function Home() {
-  return <h1>Welcome to Bitcare</h1>;
+  return <h1>Welcome to Bitcare Health solutions pvt ltd</h1>;
 }
